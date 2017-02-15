3a89f4011f971ce0ca88cdc3743b5e304cadd1b2
shawn yap <scy12a@acu.edu>
2017-02-14 23:15:20 -0600

    Added Calculation functions

--- Phase_2/Node.java
+++ Phase_2/Node.java
@@ -1,18 +1,15 @@
-package phase_2;
+package Phase_2;
 
 public class Node{
 	public String name;
 	public int cost;
 	public int value;
-	public int total;
-	
-	public Node(String name, int cost, int value, int total){
+	public double ratio;
+
+	public Node(String name, int cost, int value, double ratio){
 		this.name = name;
 		this.cost = cost;
 		this.value = value;
-		this.total = total;
-		
-		left = null;
-		right = null;
+		this.ratio = ratio;
 	}
 }
--- Phase_2/Phase2.java
+++ Phase_2/Phase2.java
@@ -6,17 +6,56 @@ import java.util.*;
 public class Phase2 {
 
 	private static List<String> tree = new ArrayList<String>();
+    private static List<Node> items = new ArrayList<Node>();
 	private static int treeHeight = 1;
 	private static int costLimit;
-	
-
 
 	public static void main(String[] args) throws Exception{
 		make_tree("Phase_2/k05.csv");
-
 		System.out.println(tree);
+
+        getOptimaltotal();
 	}
 
+    public static List<Integer> getNodeContents(String content){
+        List<Integer> holder = new ArrayList<Integer>();
+        StringTokenizer tokenizer = new StringTokenizer(content, ",");
+        int cost = 0, value = 0;
+
+        while (tokenizer.hasMoreTokens()) {
+            for (int i = 0; i < items.size(); i++) {
+                if (tokenizer.nextToken() == items.get(i).name){
+                    cost += items.get(i).cost;
+                    value += items.get(i).value;
+                }
+            }
+        }
+        holder.add(cost);
+        holder.add(value);
+        return holder;
+    }
+
+    public static void getOptimaltotal() {
+        Node newNode = new Node("", 0, 0, 0);
+        for(int i = (int)Math.pow(2,treeHeight)-1; i <= (int)Math.pow(2,treeHeight+1)-2; i++){
+            List<Integer> holder = new ArrayList<Integer>();
+            
+            // Error here
+            System.out.println(tree.get(i));
+            holder = getNodeContents(tree.get(i));
+            // Error here
+            
+                if (holder.get(0) <= costLimit && holder.get(1) > newNode.value){
+                newNode.name = tree.get(i);
+                newNode.cost = holder.get(0);
+                newNode.value = holder.get(1);
+            }
+        }
+
+        System.out.print("The Optimal solution is [" + newNode.name + "] with the cost of ");
+        System.out.print(newNode.cost + " and the value of " + newNode.value);
+
+    }   
 
 	public static void make_tree(String file) throws Exception {
 
@@ -35,18 +74,31 @@ public class Phase2 {
             String hold = "";
             String[] data = line.split(",");
 
+            String name = data[0];
+            int cost = Integer.parseInt(data[1]);
+            int value = Integer.parseInt(data[2]);
+
+            Node newNode = new Node(name, cost, value, cost/value);
+            
+            items.add(newNode);
+
+            // for (int i = 0;i < items.size(); i++) {
+            //     System.out.println(i + ": " + items.get(i).name);
+            //     System.out.println(i + ": " + items.get(i).cost);
+            //     System.out.println(i + ": " + items.get(i).value);
+            // }
+            
             for(int i = (int)Math.pow(2,treeHeight)-1; i <= (int)Math.pow(2,treeHeight+1)-2; i++){
 
             	if(i % 2 == 1){
             		tree.add(tree.get((i-1)/2));
             	}
             	else{
-            		tree.add(tree.get((i-1)/2) + data[0]);
+            		tree.add(tree.get((i-1)/2) + "," + data[0]);
             	}
             }
 
             treeHeight++;
 		}
 	}
-
 }
